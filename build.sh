ba55195efaf4ad0b9c610d5229d8b1107728b65a
Jareer Abdullah <jforeverything2007@gmail.com>
2023-02-11 19:52:00 +0100

    Update

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
+# Build Executable for Windows
 go build -o ./build/rocat.exe
+
+# Set Compile Settings for Linux
+set GOOS=linux
+set GOARCH=amd64
+set GOHOSTOS=linux
+
+# Build Executable for Linux
+go build -o ./build/rocat
