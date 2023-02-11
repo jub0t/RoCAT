85ddab4921a3841566a094bb54e14ac427d8156c
Jareer Abdullah <jforeverything2007@gmail.com>
2023-02-11 11:16:40 +0100

    Adding CLI

--- go.mod
+++ go.mod
@@ -3,3 +3,11 @@ module clob
 go 1.19
 
 require github.com/google/uuid v1.3.0
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli v1.22.12
+	github.com/urfave/cli/v2 v2.24.3 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+)
--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/urfave/cli/v2"
 )
 
 // Global Variables
@@ -11,7 +14,7 @@ const (
 	TargetGroup       = 7830839
 	AssetAPI          = `https://assetdelivery.roblox.com/v1/assetId/%v`
 	CatalogueBatchAPI = "https://catalog.roblox.com/v1/catalog/items/details"
-	GetCatalogueAPI   = `https://catalog.roblox.com/v1/search/items?category=Clothing&pxMin=5&limit=%v&salesTypeFilter=1&sortAggregation=%v&sortType=2&subcategory=%v`
+	GetCatalogueAPI   = `https://catalog.roblox.com/v1/search/items?category=Clothing&limit=%v&salesTypeFilter=1&sortAggregation=%v&sortType=2&subcategory=%v&minPrice=5`
 )
 
 // Main Function
@@ -28,42 +31,69 @@ func main() {
 		panic(err)
 	} else {
 		cookie := string(cookie_file[:])
-		if csrf, err := getCSRF(cookie); err != nil {
-			fmt.Println(`Unable to get Csrf Token, please re-check your cookie`)
-			panic(err)
-		} else {
-			if shirts, err := getCatalogue(56, 1, 120); err != nil {
-				fmt.Println(err)
-			} else {
-				if clothes, err := getClothing(GetClothesRequest{
-					Items: shirts,
-				}, cookie, csrf); err != nil {
-					fmt.Println(err)
-				} else {
-					for i := 0; i < len(clothes); i++ {
-						cloth := clothes[i]
 
-						if cloth.Price >= 5 {
-							fmt.Println(cloth.Id, cloth.ProductId, cloth.Price, cloth.Name)
+		app := &cli.App{
+			Name:  "RoCat",
+			Usage: "Roblox clothing automation tool.",
+			Commands: []*cli.Command{
+				{
+					Flags: []cli.Flag{
+						&cli.StringFlag{
+							Name:    "type",
+							Aliases: []string{"t"},
+							Usage:   "Clothing type, shirts/pants.",
+						},
+					},
+					Name:    "download",
+					Aliases: []string{"dw"},
+					Usage:   "Download classic clothing from roblox catalogue and save them for later upload",
+					Action: func(cCtx *cli.Context) error {
+						fmt.Println(cCtx.App.Flags)
+						fmt.Println(cCtx.Args())
+
+						if csrf, err := getCSRF(cookie); err != nil {
+							fmt.Println(`Unable to get Csrf Token, please re-check your cookie`)
+							panic(err)
 						} else {
-							fmt.Println("Price is less than 5 for:", cloth.Name)
-						}
-					}
+							if shirts, err := getCatalogue(56, 1, 10); err != nil {
+								fmt.Println(err)
+							} else {
+								if clothes, err := getClothing(GetClothesRequest{
+									Items: shirts,
+								}, cookie, csrf); err != nil {
+									fmt.Println(err)
+								} else {
+									for i := 0; i < len(clothes); i++ {
+										cloth := clothes[i]
 
-					test := clothes[0]
-
-					if template, err := getTemplateLink(test.Id); err != nil {
-						fmt.Println(err)
-					} else {
-						path := fmt.Sprintf(`./downloads/%v.png`, test.Id)
-						if err := downloadTemplate(template, path); err != nil {
-							fmt.Println(err)
-						} else {
-							fmt.Println(fmt.Sprintf(`Template %s Written To %v`, path, test.Id))
+										if cloth.Price >= 5 {
+											fmt.Println(cloth.Id, cloth.ProductId, cloth.Price, cloth.Name)
+											if template, err := getTemplateLink(cloth.Id); err != nil {
+												fmt.Println(err)
+											} else {
+												path := fmt.Sprintf(`./downloads/%v.png`, cloth.Id)
+												if _, err := os.Stat(path); err != nil {
+													if err := downloadTemplate(template, path); err != nil {
+														fmt.Println(err)
+													} else {
+														fmt.Println(fmt.Sprintf(`Template %s Written To %v`, path, cloth.Id))
+													}
+												}
+											}
+										}
+									}
+								}
+							}
 						}
-					}
-				}
-			}
+
+						return nil
+					},
+				},
+			},
+		}
+
+		if err := app.Run(os.Args); err != nil {
+			log.Fatal(err)
 		}
 	}
 }
