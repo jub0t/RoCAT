316626eade0ddaec91b3369077231712e276bd1c
Jareer Abdullah <jforeverything2007@gmail.com>
2023-02-12 22:07:19 +0100

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,13 +1,13 @@
-# Create the build folder
+REM Create the build folder
 mkdir ./build
 
-# Build Executable for Windows
+REM Build Executable for Windows
 go build -o ./build/rocat.exe
 
-# Set Compile Settings for Linux
+REM Set Compile Settings for Linux
 set GOOS=linux
 set GOARCH=amd64
 set GOHOSTOS=linux
 
-# Build Executable for Linux
+REM Build Executable for Linux
 go build -o ./build/rocat
